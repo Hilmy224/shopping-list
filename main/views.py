ed2333aae8bac023a394853847c1f37cd834408e
Hilmy224 <muhammad.hilmy21@ui.ac.id>
2023-10-04 14:26:16 +0700

    Tutorial 5 Selesai

--- main/views.py
+++ main/views.py
@@ -21,6 +21,10 @@ from django.contrib.auth import logout
 
 #Import for restricting access
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotFound
+
+# Import CRSF
+from django.views.decorators.csrf import csrf_exempt 
 
 #Need login before accessing the others
 @login_required(login_url='/login')
@@ -89,9 +93,6 @@ def create_product(request):
 #XML Functions
 def show_xml(request):
     data = Product.objects.all()
-
-def show_xml(request):
-    data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_xml_by_id(request, id):
@@ -101,14 +102,31 @@ def show_xml_by_id(request, id):
 #JSON Functions
 def show_json(request):
     data = Product.objects.all()
-
-def show_json(request):
-    data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+#
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
 
 #Edit Product
 def edit_product(request, id):
